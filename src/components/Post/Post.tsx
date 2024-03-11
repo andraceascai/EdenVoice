b723e420e0c3bbdb2ca4e0cee1a80df37f58cc3b
Ceascai Andra Maria <maceascai@gmail.com>
2024-03-11 14:27:10 +0200

    the content of the post shold be isnerted in the mongodb with the current date

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import axios from "axios";
 import bec from "../../frontendAssets/bec.png";
 import "./Post.css";
 
 const Post = () => {
+  const [text, setText] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("http://localhost:3001/api/posts", {
+        content: text,
+      });
+      console.log("Post created:", response.data);
+      setText("");
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+  };
+
   return (
     <>
       <div className="post-text">
@@ -11,9 +27,14 @@ const Post = () => {
         </span>
       </div>
 
-      <textarea className="form-control" id="form"></textarea>
+      <textarea
+        className="form-control"
+        id="form"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
 
-      <button type="button" className="submit-button">
+      <button type="button" className="submit-button" onClick={handleSubmit}>
         Submit
       </button>
     </>
